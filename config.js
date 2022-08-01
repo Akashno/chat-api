0ff32949033d15693400bf008f2d0fad7951ec28
Akash N O <akash@armino.in>
2022-08-01 20:28:42 +0530

    db changed to staging

--- config.js
+++ config.js
@@ -1,7 +1,12 @@
 
 let config = {
   dbUrl:
-    "mongodb+srv://noakash:arjun777@chat.cmusxed.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://noakash:arjun777@chat.cmusxed.mongodb.net/chat-staging?retryWrites=true&w=majority",
+
+
+    // WARNING: Think 34.5 times before switching to chat-live
+
+    // "mongodb+srv://noakash:arjun777@chat.cmusxed.mongodb.net/chat-live?retryWrites=true&w=majority",
 };
 
 module.exports = config;
