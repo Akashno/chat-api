7b1fc7f97802a4b5b286be82297ce3d615bb8835
Akash N O <akash@armino.in>
2022-09-26 22:51:22 +0530

    added config.js file

---
+++ config.js
@@ -0,0 +1,12 @@
+
+let config = {
+
+  testDbUrl: "mongodb+srv://noakash:arjun777@chat.cmusxed.mongodb.net/chat-testing?retryWrites=true&w=majority",
+  dbUrl:
+  //  STAGING: 
+  "mongodb+srv://noakash:arjun777@chat.cmusxed.mongodb.net/chat-staging?retryWrites=true&w=majority",
+  //  LIVE: 
+  //  "mongodb+srv://noakash:arjun777@chat.cmusxed.mongodb.net/chat-live?retryWrites=true&w=majority",
+};
+
+module.exports = config;
